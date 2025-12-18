ef4f4eed81bf71c062c633d732bc907636eb5598
Xondamir-M <xondamirmamatov0@gmail.com>
2025-12-18 11:38:13 +0100

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,26 @@
+import java.time.LocalDateTime;
+
+public class Operations {
+    Account account;
+    private double amount;
+    private TransactionType type;
+    private LocalDateTime date;
+
+
+    public enum TransactionType {
+        deposit,
+        withdraw
+    }
+
+
+    public Operations(Account account, double amount, TransactionType type) {
+        this.account = account;
+        this.amount = amount;
+        this.type = type;
+        this.date = LocalDateTime.now();
+    }
+
+    public void execute() {
+
+    }
+}
